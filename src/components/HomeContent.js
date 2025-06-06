1e04383ffccc9a199d17e179401b0c2d684cce4b
AkhilchandranR <akhilchandran771@gmail.com>
2025-06-06 11:55:44 +0530

    background update

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -4,33 +4,40 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import Jump from 'react-reveal/Jump';
 
-
 function HomeContent() {
     return (
-        <div className="homeContent">
-            <div className="homeContent__title">
-                <Fade top delay={1000}>
-                    <h1 className="homeContent__heading one">Hello.. I am Akhil</h1>
-                </Fade>
-                <Fade top delay={1400}>
-                    <h1 className="homeContent__heading two">I am a</h1>
-                </Fade>
-                <Fade top delay={1800}>
-                    <h1 className="homeContent__heading three">Full Stack Developer</h1>
-                </Fade>
-                <Fade left delay={2200}>
-                    <p className="homeContent__caption">To Turn your ideas into a reality..</p> 
-                </Fade>
-            <div className="homeContent__contact">
-                <Link to="/contact">
-                    <Jump delay={2600}>
-                        <button className="homeContent__btn">Contact Me</button>
-                    </Jump>
-                </Link>
+        <div className='background__container'>
+            <div className="homeContent">
+                <div className="homeContent__title">
+                    <Fade top delay={1000}>
+                        <h1 className="homeContent__heading one">Hello.. I am Akhil</h1>
+                    </Fade>
+                    <Fade top delay={1400}>
+                        <h1 className="homeContent__heading two">I am a</h1>
+                    </Fade>
+                    <Fade top delay={1800}>
+                        <h1 className="homeContent__heading three">Full Stack Developer</h1>
+                    </Fade>
+                    <Fade left delay={2200}>
+                        <p className="homeContent__caption">To Turn your ideas into a reality..</p> 
+                    </Fade>
+                    <div className="homeContent__contact">
+                        <Link to="/contact">
+                            <Jump delay={2600}>
+                                <button className="homeContent__btn">Contact Me</button>
+                            </Jump>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            </div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
         </div>
-    )
+    );
 }
 
-export default HomeContent
+export default HomeContent;
